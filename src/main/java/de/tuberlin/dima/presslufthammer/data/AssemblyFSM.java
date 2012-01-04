9f6734168f32191f39c5139ba163d93cc477fe8f
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2012-01-04 18:52:30 +0100

    finished actual record assembly code
    This code is rather simple, it uses all the previously added supporting
    code.

--- src/main/java/de/tuberlin/dima/presslufthammer/data/AssemblyFSM.java
+++ src/main/java/de/tuberlin/dima/presslufthammer/data/AssemblyFSM.java
@@ -4,17 +4,96 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import de.tuberlin.dima.presslufthammer.data.columnar.ColumnReader;
+import de.tuberlin.dima.presslufthammer.data.columnar.Tablet;
+import de.tuberlin.dima.presslufthammer.data.hierarchical.RecordEncoder;
+import de.tuberlin.dima.presslufthammer.data.hierarchical.RecordStore;
+
 public class AssemblyFSM {
     private List<AssemblyFSMNode> nodes;
+    private SchemaNode schema;
 
     public AssemblyFSM(SchemaNode schema) {
         nodes = Lists.newLinkedList();
+        this.schema = schema;
 
         constructFSM(schema);
     }
 
-    public AssemblyFSMNode getStartNode() {
-        return nodes.get(0);
+    public void assembleRecords(Tablet source, RecordStore target) {
+        initializeReaders(source);
+
+        AssemblyFSMNode startNode = nodes.get(0);
+        ColumnReader reader = startNode.getReader();
+        while (reader.hasNext()) {
+            assembleRecord(target);
+        }
+    }
+
+    private void adjustRecordStructure(ColumnReader reader,
+            RecordEncoder record, SchemaNode currentSchema,
+            SchemaNode lastSchema) {
+        // "moveTo"
+        SchemaNode commonAncestor = commonAncestor(currentSchema, lastSchema);
+        if (commonAncestor.equals(currentSchema)) {
+            // No need to to anything
+        }
+
+        record.returnToLevel(commonAncestor);
+        if (currentSchema.isFirstField() && commonAncestor.hasParent()) {
+            record.returnToLevel(commonAncestor.getParent());
+        }
+
+        if (reader.getNextDefinition() >= currentSchema.getMaxDefinition()) {
+            record.moveToLevel(currentSchema.getParent());
+        } else {
+            // just move to the correct level for the definition level of the
+            // reader
+            SchemaNode correctSchema = currentSchema
+                    .getSchemaForDefinitionLevel(reader.getNextDefinition());
+            record.moveToLevel(correctSchema);
+        }
+    }
+
+    private void assembleRecord(RecordStore target) {
+        RecordEncoder record = target.startRecord();
+        AssemblyFSMNode lastNode = null;
+        SchemaNode lastSchema = schema;
+        AssemblyFSMNode currentNode = nodes.get(0);
+        SchemaNode currentSchema = currentNode.getSchema();
+        ColumnReader reader = currentNode.getReader();
+
+        while (reader != null && reader.hasNext()) {
+            System.out.println("READER: " + currentSchema.getQualifiedName());
+            adjustRecordStructure(reader, record, currentSchema, lastSchema);
+            Object value = reader.getNextValue();
+            if (value != null) {
+                record.appendValue(currentSchema, value);
+            }
+            lastNode = currentNode;
+            lastSchema = lastNode.getSchema();
+            System.out.println("NEXT REP: " + reader.getNextRepetition());
+            currentNode = currentNode.getTransition(reader.getNextRepetition());
+            System.out.println(currentNode.toString());
+            reader = currentNode.getReader();
+            currentSchema = currentNode.getSchema();
+            if (currentSchema != null) {
+                record.returnToLevel(currentSchema.getParent());
+            }
+        }
+        if (reader == null) {
+            System.out.println("READER IS NULL");
+        }
+        record.returnToLevel(schema);
+        record.finalizeRecord();
+    }
+
+    private void initializeReaders(Tablet tablet) {
+        for (AssemblyFSMNode node : nodes) {
+            if (!node.isFinal()) {
+                node.setReader(tablet.getColumnReader(node.getSchema()));
+            }
+        }
     }
 
     private void constructFSM(SchemaNode schema) {
@@ -61,15 +140,12 @@ public class AssemblyFSM {
         }
     }
 
-    private int commonRepetitionLevel(SchemaNode field1, SchemaNode field2) {
-        if (field1.equals(field2)) {
-            return field1.getRepetition();
-        }
-        SchemaNode parent1 = field1.getParent();
-        SchemaNode parent2 = field2.getParent();
+    private SchemaNode commonAncestor(SchemaNode field1, SchemaNode field2) {
+        SchemaNode parent1 = field1;
+        SchemaNode parent2 = field2;
 
         while (parent1 != null && parent2 != null && !parent1.equals(parent2)) {
-            if (parent1.getRepetition() >= parent2.getRepetition()) {
+            if (parent1.getFullDefinition() >= parent2.getFullDefinition()) {
                 parent1 = parent1.getParent();
             } else {
                 parent2 = parent2.getParent();
@@ -77,9 +153,18 @@ public class AssemblyFSM {
         }
 
         if (parent1 != null && parent2 != null && parent1.equals(parent2)) {
-            return parent1.getRepetition();
+            return parent1;
         } else {
+            return null;
+        }
+    }
+
+    private int commonRepetitionLevel(SchemaNode field1, SchemaNode field2) {
+        SchemaNode commonAncestor = commonAncestor(field1, field2);
+        if (commonAncestor == null) {
             return 0;
+        } else {
+            return commonAncestor.getRepetition();
         }
     }
 
--- src/main/java/scratch/AssemblyTest.java
+++ src/main/java/scratch/AssemblyTest.java
@@ -1,26 +1,41 @@
 package scratch;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import com.google.common.io.Resources;
 
 import de.tuberlin.dima.presslufthammer.data.AssemblyFSM;
+import de.tuberlin.dima.presslufthammer.data.FieldStriper;
 import de.tuberlin.dima.presslufthammer.data.ProtobufSchemaHelper;
 import de.tuberlin.dima.presslufthammer.data.SchemaNode;
-import de.tuberlin.dima.presslufthammer.data.columnar.Tablet;
 import de.tuberlin.dima.presslufthammer.data.columnar.inmemory.InMemoryTablet;
+import de.tuberlin.dima.presslufthammer.data.hierarchical.json.JSONRecordFile;
 
 public class AssemblyTest {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException,
+            IOException {
         SchemaNode schema = ProtobufSchemaHelper.readSchema(Resources
                 .getResource("Document.proto").getFile(), "Document");
 
-        Tablet tablet = new InMemoryTablet(schema);
+        System.out.println(schema.toString());
+        JSONRecordFile records = new JSONRecordFile(schema, Resources
+                .getResource("documents.json").getFile());
+
+        InMemoryTablet inMemoryTablet = new InMemoryTablet(schema);
+        FieldStriper striper = new FieldStriper(schema, inMemoryTablet);
+        striper.dissectRecords(records);
+
+        inMemoryTablet.printColumns();
 
         AssemblyFSM fsm = new AssemblyFSM(schema);
         System.out.println(fsm.toString());
+
+        JSONRecordFile outRecords = new JSONRecordFile(schema,
+                "documents-out.json");
+
+        fsm.assembleRecords(inMemoryTablet, outRecords);
     }
 
 }

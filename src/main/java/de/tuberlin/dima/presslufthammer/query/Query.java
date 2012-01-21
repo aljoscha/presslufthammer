3dc14fa682ef6f8b9ecad2d1a58d5b58c8b8867e
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2012-01-21 23:48:47 +0100

    changed Query to use guava joiners and splitters instead of String.split
    This can handle empty fields in the query, such as :: if groupBy
    and where are empty. Also using joiners is easier and does not cause
    "," at the end of subfields.

--- src/main/java/de/tuberlin/dima/presslufthammer/query/Query.java
+++ src/main/java/de/tuberlin/dima/presslufthammer/query/Query.java
@@ -1,198 +1,205 @@
-/**
- * 
- */
 package de.tuberlin.dima.presslufthammer.query;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
 /**
  * @author feichh
+ * @author Aljoscha Krettek
  * 
  */
 public class Query {
-	private static final String SEPERATOR = ":";
-	private static final String SUBSEPERATOR = ",";
+    private static final String SEPERATOR = ":";
+    private static final String SUBSEPERATOR = ",";
+    private static final Splitter splitter = Splitter.on(SEPERATOR);
+    private static final Splitter subSplitter = Splitter.on(SUBSEPERATOR);
 
-	private byte id;
-	private Projection[] select;
-	private String from;
-	private byte part;
-	private Selection where;
-	private String[] groupBy;
+    private byte id;
+    private Projection[] select;
+    private String from;
+    private byte part;
+    private Selection where;
+    private String[] groupBy;
 
-	/**
-	 * empty constructor
-	 */
-	public Query() {
-	}
+    /**
+     * empty constructor
+     */
+    public Query() {
+    }
 
-	/**
-	 * Constructor with fields
-	 * 
-	 * @param id query ID
-	 * @param select projection clause
-	 * @param from targeted data source (table)
-	 * @param part partition of the source
-	 * @param where selection clause
-	 * @param groupBy grouping clause
-	 */
-	public Query(byte id, Projection[] select, String from, byte part,
-			Selection where, String[] groupBy) {
-		super();
-		assert (from != null);
-		this.id = id;
-		this.select = select;
-		this.from = from;
-		this.part = part;
-		this.where = where;
-		this.groupBy = groupBy;
-	}
+    /**
+     * Constructor with fields
+     * 
+     * @param id
+     *            query ID
+     * @param select
+     *            projection clause
+     * @param from
+     *            targeted data source (table)
+     * @param part
+     *            partition of the source
+     * @param where
+     *            selection clause
+     * @param groupBy
+     *            grouping clause
+     */
+    public Query(byte id, Projection[] select, String from, byte part,
+            Selection where, String[] groupBy) {
+        super();
+        assert (from != null);
+        this.id = id;
+        this.select = select;
+        this.from = from;
+        this.part = part;
+        this.where = where;
+        this.groupBy = groupBy;
+    }
 
-	/**
-	 * expects Query.toString().getBytes() as input
-	 * 
-	 * @param bytes bytes of a String representing a Query
-	 */
-	public Query(byte[] bytes) {
-		new Query(new String(bytes));
-	}
+    /**
+     * expects Query.toString().getBytes() as input
+     * 
+     * @param bytes
+     *            bytes of a String representing a Query
+     */
+    public Query(byte[] bytes) {
+        this(new String(bytes));
+    }
 
-	/**
-	 * expects Query.toString() as input
-	 * 
-	 * @param query String representation of a Query
-	 */
-	public Query(String query) {
-		String[] split = query.split(SEPERATOR);
-		assert (split.length != 6);
-		// ID
-		this.id = Byte.parseByte(split[0]);
-		// SELECT
-		String[] selects = split[1].split(SUBSEPERATOR);
-		this.select = new Projection[selects.length];
-		for (int i = 0; i < selects.length; i++) {
-			this.select[i] = new Projection(selects[i]);
-		}
-		// FROM
-		this.from = split[2];
-		// PART
-		this.part = Byte.parseByte(split[3]);
-		// WHERE
-		this.where = new Selection(split[4]);
-		// GROUP BY
-		String[] groups = split[5].split(SUBSEPERATOR);
-		this.groupBy = new String[groups.length];
-		for (int i = 0; i < groups.length; i++) {
-			this.groupBy[i] = groups[i];
-		}
-	}
+    /**
+     * expects Query.toString() as input
+     * 
+     * @param query
+     *            String representation of a Query
+     */
+    public Query(String query) {
+        Iterator<String> splitIter = splitter.split(query).iterator();
+        // ID
+        this.id = Byte.parseByte(splitIter.next());
+        // SELECT
+        Iterable<String> selectIter = subSplitter.split(splitIter.next());
+        List<Projection> selectList = Lists.newLinkedList();
+        for (String select : selectIter) {
+            selectList.add(new Projection(select));
+        }
+        this.select = selectList.toArray(new Projection[selectList.size()]);
+        // FROM
+        this.from = splitIter.next();
+        // PART
+        this.part = Byte.parseByte(splitIter.next());
+        // WHERE
+        this.where = new Selection(splitIter.next());
+        // GROUP BY
+        Iterable<String> groupIter = subSplitter.split(splitIter.next());
+        List<String> groupByList = Lists.newLinkedList();
+        for (String groupBy : groupIter) {
+            groupByList.add(groupBy);
+        }
+        this.groupBy = groupByList.toArray(new String[groupByList.size()]);
+    }
 
-	/**
-	 * @return the id
-	 */
-	public byte getId() {
-		return id;
-	}
+    /**
+     * @return the id
+     */
+    public byte getId() {
+        return id;
+    }
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(byte id) {
-		this.id = id;
-	}
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(byte id) {
+        this.id = id;
+    }
 
-	/**
-	 * @return the select
-	 */
-	public Projection[] getSelect() {
-		return select;
-	}
+    /**
+     * @return the select
+     */
+    public Projection[] getSelect() {
+        return select;
+    }
 
-	/**
-	 * @param select
-	 *            the select to set
-	 */
-	public void setSelect(Projection[] select) {
-		this.select = select;
-	}
+    /**
+     * @param select
+     *            the select to set
+     */
+    public void setSelect(Projection[] select) {
+        this.select = select;
+    }
 
-	/**
-	 * @return the from
-	 */
-	public String getFrom() {
-		return from;
-	}
+    /**
+     * @return the from
+     */
+    public String getFrom() {
+        return from;
+    }
 
-	/**
-	 * @param from
-	 *            the from to set
-	 */
-	public void setFrom(String from) {
-		this.from = from;
-	}
+    /**
+     * @param from
+     *            the from to set
+     */
+    public void setFrom(String from) {
+        this.from = from;
+    }
 
-	/**
-	 * @return the part
-	 */
-	public byte getPart() {
-		return part;
-	}
+    /**
+     * @return the part
+     */
+    public byte getPart() {
+        return part;
+    }
 
-	/**
-	 * @param part
-	 *            the part to set
-	 */
-	public void setPart(byte part) {
-		this.part = part;
-	}
+    /**
+     * @param part
+     *            the part to set
+     */
+    public void setPart(byte part) {
+        this.part = part;
+    }
 
-	/**
-	 * @return the where
-	 */
-	public Selection getWhere() {
-		return where;
-	}
+    /**
+     * @return the where
+     */
+    public Selection getWhere() {
+        return where;
+    }
 
-	/**
-	 * @param where
-	 *            the where to set
-	 */
-	public void setWhere(Selection where) {
-		this.where = where;
-	}
+    /**
+     * @param where
+     *            the where to set
+     */
+    public void setWhere(Selection where) {
+        this.where = where;
+    }
 
-	/**
-	 * @return the groupBy
-	 */
-	public String[] getGroupBy() {
-		return groupBy;
-	}
+    /**
+     * @return the groupBy
+     */
+    public String[] getGroupBy() {
+        return groupBy;
+    }
 
-	/**
-	 * @param groupBy
-	 *            the groupBy to set
-	 */
-	public void setGroupBy(String[] groupBy) {
-		this.groupBy = groupBy;
-	}
+    /**
+     * @param groupBy
+     *            the groupBy to set
+     */
+    public void setGroupBy(String[] groupBy) {
+        this.groupBy = groupBy;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		// TODO
-		String string = id + SEPERATOR;
-		for (Projection p : select) {
-			string += p.toString() + SUBSEPERATOR;
-		}
-		string += SEPERATOR + from;
-		string += SEPERATOR + part;
-		string += SEPERATOR + where + SEPERATOR;
-		for (String g : groupBy) {
-			string += g + SUBSEPERATOR;
-		}
-		return string;
-	}
+    @Override
+    public String toString() {
+        String string = id + SEPERATOR;
+        Joiner subJoin = Joiner.on(SUBSEPERATOR);
+        string += subJoin.join(select);
+        string += SEPERATOR + from;
+        string += SEPERATOR + part;
+        string += SEPERATOR + where + SEPERATOR;
+        string += subJoin.join(groupBy);
+        return string;
+    }
 }
